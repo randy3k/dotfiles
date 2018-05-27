92f7eba16c81270bffe295778e61452dab13f84d
Randy Lai <randy.cs.lai@gmail.com>
2018-05-27 18:07:55 -0400

    Update dotfiles at Sun May 27 22:07:55 UTC 2018

--- .ipython/profile_default/startup/20-keyboard.py
+++ .ipython/profile_default/startup/20-keyboard.py
@@ -1,8 +1,11 @@
-from IPython import get_ipython
+import IPython
 
+try:
+    if IPython.__version__ > "6":
+        ip = IPython.get_ipython()
+        registry = ip.pt_cli.application.key_bindings_registry
 
-ip = get_ipython()
-registry = ip.pt_cli.application.key_bindings_registry
-
-handler = next(kb.handler for kb in registry.key_bindings if kb.handler.__name__ == "newline_autoindent")
-registry.remove_binding(handler)
+        handler = next(kb.handler for kb in registry.key_bindings if kb.handler.__name__ == "newline_autoindent")
+        registry.remove_binding(handler)
+except:
+    pass
